76d480816933b642d82953d4d9ffb2690eb793b0
subham <andharalauri@gmail.com>
2023-01-23 10:12:20 +0530

    added

--- unit-5/Practice_Core_Java/src/com/masai/day_1/Demo.java
+++ unit-5/Practice_Core_Java/src/com/masai/day_1/Demo.java
@@ -4,7 +4,12 @@ public class Demo {
 	
 	public static void main(String[] args) {
 		
+	
+//		Wrapper: Boolean,Byte,Character,Double,Float,Integer,Long,Short
+//		Primitive: boolean,byte,char ,double, float, int , long,short
 		
+		Integer number2 = new Integer("55");
+		System.out.println(number2);
 		
 	}
 
