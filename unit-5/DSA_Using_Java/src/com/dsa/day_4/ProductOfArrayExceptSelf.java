bdd8fb36024418645160d957c793055f202a793e
subham <andharalauri@gmail.com>
2023-01-31 19:05:46 +0530

    added

---
+++ unit-5/DSA_Using_Java/src/com/dsa/day_4/ProductOfArrayExceptSelf.java
@@ -0,0 +1,69 @@
+package com.dsa.day_4;
+import java.util.*;
+import java.io.*;
+
+public class ProductOfArrayExceptSelf {
+
+}
+
+class Main{
+    static class FastReader
+  {
+    BufferedReader br;
+    StringTokenizer st;
+    public FastReader()
+    {
+      br = new BufferedReader(new InputStreamReader(System.in));
+    }
+    String next()
+    {
+      while (st == null || !st.hasMoreElements()){
+        try { st = new StringTokenizer(br.readLine()); }
+        catch (IOException e) { e.printStackTrace(); }
+      }
+      return st.nextToken();
+    }
+    int nextInt() { return Integer.parseInt(next()); }
+    long nextLong() { return Long.parseLong(next()); }
+    double nextDouble(){ return Double.parseDouble(next()); }
+    String nextLine(){
+      String str = "";
+      try { str = br.readLine(); }
+      catch (IOException e) { e.printStackTrace(); }
+      return str;
+    }
+  }
+  public static void main(String[] args){
+     FastReader sc = new FastReader();
+      int tc = sc.nextInt();
+        while(tc-- > 0){
+            int n = sc.nextInt();
+            int[] arr = new int[n];
+            for(int i=0;i<n;i++){
+                arr[i] = sc.nextInt();
+            }
+            StringBuilder sb = new StringBuilder();
+            int[] ans = new int[n];
+            ans = productExceptSelf(arr);
+            for(int i=0;i<n;i++){
+                sb.append(ans[i]+" ");
+            }
+            System.out.println(sb);
+  }
+}
+
+    public static int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] res = new int[n];
+        res[0] = 1;
+        for (int i = 1; i < n; i++) {
+            res[i] = res[i - 1] * nums[i - 1];
+        }
+        int right = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            res[i] *= right;
+            right *= nums[i];
+        }
+        return res;
+    }
+}
