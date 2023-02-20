b23c7ac062d677bc694307efc8c53aca50d00297
subham <andharalauri@gmail.com>
2023-02-20 09:55:26 +0545

    added

--- unit-5/Maven_Practice_1/src/main/java/com/masai/Demo.java
+++ unit-5/Maven_Practice_1/src/main/java/com/masai/Demo.java
@@ -4,26 +4,19 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Demo {
 
 	public static void main(String[] args) {
-
-		EntityManagerFactory emf = Persistence.createEntityManagerFactory("studentUnit");
-		EntityManager em = emf.createEntityManager();
-
-//		Student student = em.find(Student.class, 10);
-//		if (student != null)
-//			System.out.println(student);
-//		else
-//			System.out.println("Student does not exist");
-//		em.close();
-		
-		Student st1 = new Student(30, "subham", 900);
-		em.getTransaction().begin();
-		em.persist(st1);
-		em.getTransaction().commit();
-		em.clear();
-		System.out.println("Done....");
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
+		Demo d = ctx.getBean("mb",Demo.class);
+		d.Method();
+	}
+	
+	public void Method() {
+		System.out.println("Hi man");
 	}
 
 }
